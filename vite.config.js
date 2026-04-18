7a156b0120a73795ab9fb02ed349aa3cd77c2311
ashutosh-swami30 <125626143+ashutosh-swami30@users.noreply.github.com>
2026-04-18 12:08:53 +0530

    Remove unused import of T from tldraw

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { T } from 'tldraw'
 
 // https://vite.dev/config/
 export default defineConfig({
