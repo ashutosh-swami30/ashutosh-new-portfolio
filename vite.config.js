6f9486dedab73d230d9888b30a68004e8a251125
ashutosh-swami30 <125626143+ashutosh-swami30@users.noreply.github.com>
2026-07-09 11:30:59 +0000

    Revert to state where aurora was absent

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,9 +8,4 @@ export default defineConfig({
     react(), 
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
